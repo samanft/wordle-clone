2e4ecc044a0b70401bb7dfe97ae6efdc6d453541
samanft <poopeekaska@gmail.com>
2024-06-26 14:15:22 +0200

    added full word checking functionality (green and yellow)

--- src/components/Wordle.vue
+++ src/components/Wordle.vue
@@ -1,27 +1,30 @@
 <script setup>
-import { ref, onMounted } from 'vue';
+import { ref, onMounted } from "vue";
 
 const inputs = ref([null, null, null, null, null]);
 
-const woord = ['h', 'e', 'l', 'l', 'o'];
+const word = ["h", "e", "l", "l", "o"];
+const letterStates = ref(new Array(word.length).fill("")); // Initialize with empty states
 
 const checkWord = () => {
-  const inputValues = inputs.value.map(input => input.value);
-  if (inputValues.join('') === woord.join('')) {
-    alert('You guessed the word!');
-  } else {
-    alert('Try again!');
+  const inputValues = inputs.value.map((input) => input.value);
+  for (let i = 0; i < inputValues.length; i++) {
+    if (inputValues[i] === word[i]) {
+      letterStates.value[i] = "correct";
+    } else if (word.includes(inputValues[i])) {
+      letterStates.value[i] = "wrong-position";
+    }
   }
 };
 
-const logCharCount = (e) => {
-  if (!e.key.match(/^[a-zA-Z]$/) && e.key !== 'Backspace') {
+const handleInput = (e) => {
+  if (!e.key.match(/^[a-zA-Z]$/) && e.key !== "Backspace") {
     e.preventDefault();
     return;
   }
 
   console.log(e.target.value.length);
-  const index = inputs.value.findIndex(input => input === e.target);
+  const index = inputs.value.findIndex((input) => input === e.target);
   if (e.target.value.length === 1) {
     if (index === inputs.value.length - 1) {
       checkWord();
@@ -39,15 +42,21 @@ onMounted(() => {
   }
 });
 
-const inputCount = woord.length; // Assuming you want 5 inputs
-
-
+const inputCount = word.length;
 </script>
 
 <template>
   <div>
-    <input v-for="index in inputCount" :key="index" type="text" maxlength="1" @keyup="logCharCount"
-           :ref="el => inputs[index - 1] = el" />
+    <input
+      v-for="index in inputCount"
+      :key="index"
+      type="text"
+      maxlength="1"
+      @keyup="handleInput"
+      :ref="(el) => (inputs[index - 1] = el)"
+      :class="letterStates[index - 1]"
+      :aria-label="`Letter ${index}`"
+    />
   </div>
 </template>
 
@@ -56,5 +65,20 @@ input[type="text"] {
   width: 50px; /* Makes the input square in shape */
   height: 50px;
   text-align: center; /* Centers the text */
+  background-color: #3a3a3c;
+  border: none;
+  margin-right: 5px;
+}
+
+input[type="text"].wrong-position {
+  background-color: #b59f3b;
+}
+
+input[type="text"].correct {
+  background-color: #538d4e;
+}
+
+* {
+  color: white;
 }
 </style>
